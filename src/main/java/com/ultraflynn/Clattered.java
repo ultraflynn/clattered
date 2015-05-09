fee4532d36dab047d3c824bba0db2421957b332e
mbiggin <matthew.biggin@gmail.com>
2015-05-09 10:53:36 +0100

    Added support for multiple users

--- src/main/java/com/ultraflynn/Clattered.java
+++ src/main/java/com/ultraflynn/Clattered.java
@@ -1,7 +1,9 @@
 package com.ultraflynn;
 
+import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Multimap;
 import org.joda.time.DateTime;
 
 import java.util.List;
@@ -9,17 +11,18 @@ import java.util.List;
 public class Clattered {
     private static DateFormat DATE_FORMAT = new DateFormat();
 
-    private final List<Message> messages = Lists.newArrayList();
+    private final Multimap<String, Message> usersMessages = ArrayListMultimap.create();
 
     public void publish(String user, String text) {
         Message message = new Message(text);
-        messages.add(message);
+        usersMessages.put(user, message);
     }
 
     public List<String> timeline(String user) {
         DateTime now = DateTime.now();
 
         ImmutableList.Builder<String> builder = ImmutableList.builder();
+        List<Message> messages = ImmutableList.copyOf(usersMessages.get(user));
         for (Message message : Lists.reverse(messages)) {
             String elapsed = DATE_FORMAT.format(message.timestamp, now);
 
