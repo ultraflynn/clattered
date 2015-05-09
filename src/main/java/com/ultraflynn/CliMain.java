f0d149cd61e301aef23140c47f9b76a1cfb91cdf
mbiggin <matthew.biggin@gmail.com>
2015-05-09 12:59:48 +0100

    Added build config for command line

--- src/main/java/com/ultraflynn/CliMain.java
+++ src/main/java/com/ultraflynn/CliMain.java
@@ -1,9 +1,21 @@
 package com.ultraflynn;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class CliMain {
     private final Clattered clattered = new Clattered();
 
     public static void main(String[] args) {
-
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            String s;
+            while ((s = in.readLine()) != null && s.length() != 0) {
+                System.out.println(s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
