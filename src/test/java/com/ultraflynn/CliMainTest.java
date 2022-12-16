e6393e22f0133d2c89dab47b7ab66c4ca46a0624
Matt Biggin <mattbiggin@runbox.com>
2022-12-16 12:29:41 +0000

    Updated Mockito dependency

--- src/test/java/com/ultraflynn/CliMainTest.java
+++ src/test/java/com/ultraflynn/CliMainTest.java
@@ -8,7 +8,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.InOrder;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.BufferedReader;
 import java.io.IOException;
